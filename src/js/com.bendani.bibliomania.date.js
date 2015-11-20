5a70adad0900cb73cc80b167261cf4cc4820300e
David Maes <david.s.maes@gmail.com>
2015-11-20 20:36:26 +0100

    added date directive

--- src/js/com.bendani.bibliomania.date.js
+++ src/js/com.bendani.bibliomania.date.js
@@ -39,7 +39,7 @@ angular
                 };
 
                 function isBlank(string){
-                    return string === undefined || string.trim() === '';
+                    return string === undefined || string === '';
                 }
 
                 init();
