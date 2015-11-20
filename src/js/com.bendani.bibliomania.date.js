175d1366083b45658628b074bb8101db9be499ef
David Maes <david.s.maes@gmail.com>
2015-11-20 20:17:45 +0100

    added date directive

---
+++ src/js/com.bendani.bibliomania.date.js
@@ -0,0 +1,49 @@
+angular
+    .module('com.bendani.bibliomania.date', [])
+    .directive('date', function (){
+        return {
+            scope: {
+                label: '@',
+                dateModel: "=ngModel",
+                required: '@'
+            },
+            restrict: "E",
+            templateUrl: "../BiblioMania/views/partials/date-directive.html",
+            template: '<div ng-include="getTemplateUrl()"></div>',
+            controller: ['$rootScope', '$scope', function($rootScope, $scope) {
+
+                function init(){
+                    if($scope.dateModel === undefined){
+                        $scope.dateModel = {};
+                    }
+                }
+
+                $scope.getTemplateUrl = function(){
+                    return $rootScope.baseUrl + "packages/bendani/php-common/uiframework/date-directive.html";
+                };
+
+                $scope.isYearRequired = function(){
+                    if($scope.required === true){
+                        return true;
+                    }
+                    if($scope.dateModel === undefined){
+                        return false;
+                    }
+                    return !isBlank($scope.dateModel.day) || !isBlank($scope.dateModel.month);
+                };
+
+                $scope.isMonthRequired = function(){
+                    if($scope.dateModel === undefined){
+                        return false;
+                    }
+                    return !isBlank($scope.dateModel.day);
+                };
+
+                function isBlank(string){
+                    return string !== undefined && string !== '';
+                }
+
+                init();
+            }]
+        };
+    });
