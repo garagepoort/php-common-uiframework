aff8d7fcd3a05c0157cb9aed2c3581823d383e67
David Maes <david.s.maes@gmail.com>
2016-04-02 23:43:44 +0200

    Fix bug in multiselect. Multiselect now check based on key if value already selected

--- src/js/php.common.uiframework.multiselect.js
+++ src/js/php.common.uiframework.multiselect.js
@@ -1,42 +1,61 @@
 angular
     .module('com.bendani.php.common.uiframework')
-    .directive('multiSelect', function (){
+    .directive('multiSelect', function () {
         return {
             scope: {
                 allItems: "=",
                 selectedItems: "=",
                 onSelect: "&",
-                onDeselect: "&",
+                onDeselect: "&"
             },
             restrict: "E",
             templateUrl: 'packages/bendani/php-common/uiframework/multi-select.html',
-            controller: ['$scope', function($scope) {
-                function init(){
+            controller: ['$scope', function ($scope) {
+                function init() {
                     $scope.data = {};
                     $scope.data.searchItemsQuery = "";
                 }
 
-                $scope.selectItem = function selectItem(item){
-                    var index = $scope.selectedItems.indexOf(item);
-                    if(index > -1){
-                        $scope.selectedItems.splice(index, 1);
-                        if($scope.onDeselect){
+                $scope.selectItem = function selectItem(item) {
+                    if ($scope.itemIsSelected(item)) {
+                        removeItem(item);
+                        if ($scope.onDeselect) {
                             $scope.onDeselect({item: item});
                         }
-                    }else{
+                    } else {
                         $scope.selectedItems.push(item);
-                        if($scope.onSelect){
+                        if ($scope.onSelect) {
                             $scope.onSelect({item: item});
                         }
                     }
                 };
 
-                $scope.search = function(item){
-                    if ( (item.key.toLowerCase().indexOf($scope.data.searchItemsQuery) !== -1)){
+                $scope.itemIsSelected = function (item) {
+                    var selectedItem = $.grep($scope.selectedItems, function (si) {
+                        return si.key == item.key;
+                    });
+
+                    return selectedItem.length > 0;
+                };
+
+                $scope.search = function (item) {
+                    if ((item.key.toLowerCase().indexOf($scope.data.searchItemsQuery) !== -1)) {
                         return true;
                     }
                     return false;
                 };
+
+                function removeItem(item){
+                    var indexToRemove = -1;
+                    for(var i = 0; i < $scope.selectedItems.length; i++){
+                        if($scope.selectedItems[i].key == item.key){
+                            indexToRemove = i;
+                        }
+                    }
+                    if(indexToRemove != -1){
+                        $scope.selectedItems.splice(indexToRemove, 1);
+                    }
+                }
 
                 init();
             }]
