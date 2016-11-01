ad054135ff2d1f84f5474b065898bc49672d4459
David Maes <david.s.maes@gmail.com>
2016-11-01 08:55:33 +0100

    added compare to directive

---
+++ src/js/com.bendani.php.common.uiframework.compare.to.directive.js
@@ -0,0 +1,24 @@
+(function(){
+    'use strict';
+    angular.module('com.bendani.php.common.uiframework')
+        .directive("compareTo", compareTo);
+
+    function compareTo() {
+        return {
+            require: "ngModel",
+            scope: {
+                otherModelValue: "=compareTo"
+            },
+            link: function(scope, element, attributes, ngModel) {
+
+                ngModel.$validators.compareTo = function(modelValue) {
+                    return modelValue == scope.otherModelValue;
+                };
+
+                scope.$watch("otherModelValue", function() {
+                    ngModel.$validate();
+                });
+            }
+        };
+    }
+})();
